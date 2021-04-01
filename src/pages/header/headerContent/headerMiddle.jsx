0bbad5a7e22e281234622cdf3bd9f09cedbeea49
Orifjon Yunusjonov <mryediniofficial9924@gmail.com>
2021-04-01 10:03:13 +0500

    logo change

--- src/pages/header/headerContent/headerMiddle.jsx
+++ src/pages/header/headerContent/headerMiddle.jsx
@@ -9,8 +9,11 @@ const HeaderMiddle = () => {
                 <div className={"container"}>
                     <div className={"row"}>
                         <div className={"col-md-4 col-sm-4 col-xs-12"}>
-                            <div className={"logo"}>
-                                <Link to={"/"}><img src={logo} alt="logo"/></Link>
+                            <div className={"logo my-1"}>
+                                <Link to={"/"} style={{color: "black",fontWeight:"bold",fontSize:"60px", }}>
+                                <span style={{color:"#EC5F25"}}>DOIM</span>.UZ
+                                    {/*<img src={logo} alt="logo"/>*/}
+                                </Link>
                             </div>
                         </div>
                         <div className={"col-md-8 col-sm-8 col-xs-12"}>
